04aca53f89a16397122912ce9783e408624e30d6
Meno <menolycasch2006@gmail.com>
2026-07-24 17:05:18 +0530

    Commit 13 : Footer + Adjustments

---
+++ script.js
@@ -0,0 +1,40 @@
+const container = document.getElementById("repo-container");
+
+if (container) {
+
+    fetch("https://api.github.com/users/meno-cv/repos")
+        .then(response => response.json())
+        .then(repos => {
+
+            repos.slice(0, 6).forEach(repo => {
+
+                container.innerHTML += `
+
+                <div class="col-lg-4">
+
+                    <div class="repo-card">
+
+                        <h4>${repo.name}</h4>
+
+                        <p>${repo.description ?? "No description available."}</p>
+
+                        <a href="${repo.html_url}"
+                           target="_blank"
+                           rel="noopener noreferrer"
+                           class="btn project-btn">
+
+                           View Repository
+
+                        </a>
+
+                    </div>
+
+                </div>
+
+                `;
+
+            });
+
+        });
+
+}
